ee5a9f654ed8d83948354208f18893b028a2de93
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-08-17 14:29:40 -0700

    add

---
+++ app.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('.wrapper')
+const registerlink = document.querySelector('.register-link')
+const loginlink = document.querySelector('.login-link')
+
+registerlink.onclick = ()=>{
+    wrapper.classList.add("active")
+}
